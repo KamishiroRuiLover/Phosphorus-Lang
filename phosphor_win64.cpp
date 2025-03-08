0333176ef35af46c02f27da94afb4bc3e9c3a18a
furriesRus <shousejackson@gmail.com>
2025-03-07 21:39:28 -0600

    Added token class

--- phosphor_win64.cpp
+++ phosphor_win64.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+class Token {
+    public:
+        std::string type;
+        std::string value;
+};
 
 int main() {
     std::cout << "hi :3\n";
